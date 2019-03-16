2f0b026b1f5a26c947a6f52c84f35133479d62e2
Jeffrey Kaplan (OP-Laptop) <jeffmankap@gmail.com>
2019-03-15 19:36:43 -0600

    initial design testing

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req,res) => {
     res.render('index', {
-        title: 'Subscript',
+        title: 'Welcome',
         name: 'Jeffrey Kaplan'
     })
 })
