6921aadb619bf59e2c2006e6b9038789beb5f761
Jeffrey Kaplan (OP-Laptop) <jeffmankap@gmail.com>
2019-03-20 15:30:27 -0600

    removing console logs

--- public/js/collapse.js
+++ public/js/collapse.js
@@ -2,8 +2,8 @@ ready(() => {
     let collapsers = document.getElementsByClassName('collapsed');
     for(let collapser of collapsers) {
         let trigger = collapser.querySelectorAll('.collapse-trigger');
-        console.log('trigger set', trigger);
-        console.log('collapser set', collapser);
+        // console.log('trigger set', trigger);
+        // console.log('collapser set', collapser);
 		if(trigger && trigger.length) {
 			trigger[0].onclick = () => {
 				toggleClass(collapser, 'collapsed');
