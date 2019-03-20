3b986346ad81239e62837dc8a5384f6ee2ec61fb
Jeffrey Kaplan (OP-Laptop) <jeffmankap@gmail.com>
2019-03-20 12:28:46 -0600

    scroll progress bugfix

--- public/js/section-scroll-progress.js
+++ public/js/section-scroll-progress.js
@@ -1,37 +1,38 @@
 
 // 
+let mainNavLinks;
 
-let mainNavLinks = document.querySelectorAll(".nav a, .logo");
-let mainSections = document.querySelectorAll(".page-content");
-
-let lastId;
-let cur = [];
-
-const applyProgressClasses = () => {
-    let fromTop = window.scrollY;
-    mainNavLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
-    
-        if (
-          section.offsetTop <= fromTop + 354 &&
-          section.offsetTop + section.offsetHeight - 354 > fromTop
-        ) {
-          link.classList.add("current");
-          section.classList.add("current");
-        //   section.focus();
-        } else {
-          link.classList.remove("current");
-          section.blur();
-          section.classList.remove("current");
-        //   section.focus();
-        }
-    });
+const scrollProgressInit = () => {
+  mainNavLinks = document.querySelectorAll(".nav a");
+  applyProgressClasses();
 }
 
-// need to add a scroll buffer with lodash
-window.addEventListener("scroll", event => {
-  applyProgressClasses()
-});
+const applyProgressClasses = () => {
+  
+  let fromTop = window.scrollY;
+  mainNavLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+  
+      if (
+        section.offsetTop <= fromTop + 354 &&
+        section.offsetTop + section.offsetHeight - 354 > fromTop
+      ) {
+        link.classList.add("current");
+        section.classList.add("current");
+      //   section.focus();
+      } else {
+        link.classList.remove("current");
+        section.blur();
+        section.classList.remove("current");
+      //   section.focus();
+      }
+  });
+}
+
+  // need to add a scroll buffer with lodash
+  window.addEventListener("scroll", event => {
+    applyProgressClasses()
+  });
 
 // run on load
-ready(applyProgressClasses);
+ready(scrollProgressInit);
